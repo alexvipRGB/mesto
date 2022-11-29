ccade0ef30208030c5cb643aa1051667a755d0a1
alexvipRGB <93926733+alexvipRGB@users.noreply.github.com>
2022-11-30 01:25:11 +0300

    Обновление

--- script/index.js
+++ script/index.js
@@ -1,25 +1,45 @@
-const headerButton = document.querySelector('.profile__submit-btn');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
+const likeButtons = document.querySelectorAll('.element__like');
+// Находим форму в DOM
+const popupForm = document.getElementById('popupForm');
+// Находим поля формы в DOM
+const nameInput = document.getElementById('nameInput');
+const jobInput = document.getElementById('jobInput'); // Воспользуйтесь инструментом .querySelector()
+var button = document.getElementById('button');
+const nametext = document.querySelector('.profile__title');
+const job = document.querySelector('.profile__paragrah');
 
-headerButton.addEventListener('click', openPopup);
-popupClose.addEventListener('click', closePopup);
-
-
-function openPopup(event) {
-    event.preventDefault();
+function openPopup() {
+    nameInput.value = nametext.textContent;
+    jobInput.value = job.textContent;
     popup.classList.add('popup_opened');
+    console.log(nametext.textContent);
+    console.log(nameInput.value);
+
+}
+
+function handleFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    nametext.textContent = nameInput.value;// Выберите элементы, куда должны быть вставлены значения полей
+    job.textContent = jobInput.value; // Вставьте новые значения с помощью textContent
+    console.log(nametext.textContent);
+    console.log(nameInput.value);
+    closePopup();
 }
 
 function closePopup() {
     popup.classList.remove('popup_opened');
+
 }
 
-popup.addEventListener('click', function (event) {
-    if (!event.defaultPrevented) {
-        closePopup();
-    }
-})
-document.querySelector('.popup__container').addEventListener('click', function (event) {
-    event.preventDefault();
-})
+function like(clickLike) {
+    clickLike.classList.toggle('element__like_active');
+}
+for (let i = 0; i < likeButtons.length; i++) {
+    likeButtons[i].addEventListener('click', () => like(likeButtons[i]));
+}
+
+button.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', handleFormSubmit);
