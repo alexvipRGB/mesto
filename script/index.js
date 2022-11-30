bc7c9daf4068536eb543230df40b351d53c3d928
alexvipRGB <93926733+alexvipRGB@users.noreply.github.com>
2022-11-30 18:05:49 +0300

    Обновление

--- script/index.js
+++ script/index.js
@@ -1,12 +1,11 @@
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
-const likeButtons = document.querySelectorAll('.element__like');
 // Находим форму в DOM
 const popupForm = document.getElementById('popupForm');
 // Находим поля формы в DOM
 const nameInput = document.getElementById('nameInput');
 const jobInput = document.getElementById('jobInput'); // Воспользуйтесь инструментом .querySelector()
-var button = document.getElementById('button');
+const buttonProf = document.getElementById('buttonProf');
 const nametext = document.querySelector('.profile__title');
 const job = document.querySelector('.profile__paragrah');
 
@@ -14,17 +13,12 @@ function openPopup() {
     nameInput.value = nametext.textContent;
     jobInput.value = job.textContent;
     popup.classList.add('popup_opened');
-    console.log(nametext.textContent);
-    console.log(nameInput.value);
-
 }
 
 function handleFormSubmit(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     nametext.textContent = nameInput.value;// Выберите элементы, куда должны быть вставлены значения полей
     job.textContent = jobInput.value; // Вставьте новые значения с помощью textContent
-    console.log(nametext.textContent);
-    console.log(nameInput.value);
     closePopup();
 }
 
@@ -33,13 +27,6 @@ function closePopup() {
 
 }
 
-function like(clickLike) {
-    clickLike.classList.toggle('element__like_active');
-}
-for (let i = 0; i < likeButtons.length; i++) {
-    likeButtons[i].addEventListener('click', () => like(likeButtons[i]));
-}
-
-button.addEventListener('click', openPopup);
+buttonProf.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', handleFormSubmit);
