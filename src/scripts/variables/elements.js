9e137ce4aee12c834813ad15bcb3cd09c4c7f5e7
alexvipRGB <93926733+alexvipRGB@users.noreply.github.com>
2023-02-08 10:28:43 +0300

    8Спринт

--- src/scripts/variables/elements.js
+++ src/scripts/variables/elements.js
@@ -1,5 +1,4 @@
 const page = document.querySelector('.page');
-
 const profile = page.querySelector('.profile');
 const buttonNewProfil = profile.querySelector('.profile__submit-btn');
 const buttonNewMesto = profile.querySelector('.profile__button');
@@ -7,7 +6,6 @@ const buttonNewMesto = profile.querySelector('.profile__button');
 const popupProfilForm = document.forms.popupProfilForm;
 const nameInput = popupProfilForm.elements.nameInput
 const jobInput = popupProfilForm.elements.jobInput
-
 const popupFormNewMesto = document.forms.popupFormNewMesto;
 
 export { profile, buttonNewProfil, buttonNewMesto, popupProfilForm, nameInput, jobInput, popupFormNewMesto }
